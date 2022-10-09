fb3f2994fdbd33ee13dcf8b6cf468f606ba9a4b7
webpawan <pawansaini66477@gmail.com>
2022-10-10 00:05:24 +0530

    build fix

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,28 +4,28 @@ const Main = () => {
   return (
     <>
     
-    <div class="container-fluid bg">
+    <div className="container-fluid bg">
 
-<div class="row d-flex justify-content-center align-items-center vh-100 ">
+<div className="row d-flex justify-content-center align-items-center vh-100 ">
 
 
   
-  <div class="col-12 col-sm-10 col-lg-7 centerDiv p-2 mx-4 my-4" id="show">
+  <div className="col-12 col-sm-10 col-lg-7 centerDiv p-2 mx-4 my-4" id="show">
   
-   <div class="search w-75 mx-auto text-center my-3 p-4">
-     <h3 class="text-capitalize "> <span id="day">thuesdat</span> <span id="time">10:00</span>  </h3> 
-    <input type="text" class="myinput w-75  " id="inputTag" placeholder="search your location"/>
+   <div className="search w-75 mx-auto text-center my-3 p-4">
+     <h3 className="text-capitalize "> <span id="day">thuesdat</span> <span id="time">10:00</span>  </h3> 
+    <input type="text" className="myinput w-75  " id="inputTag" placeholder="search your location"/>
    </div>
     
     
-<div class=" d-sm-flex text-center justify-content-center align-items-end ">
+<div className=" d-sm-flex text-center justify-content-center align-items-end ">
 
-  <h1 class="temp " id="temp">0<sup>&#176;c</sup></h1>  
- <p class=""><h5 class="mx-2 text-white lead" id="max"> max  </h5>  <h5 class="mx-1 lead text-white" id="min">min</h5> </p>
+  <h1 className="temp " id="temp">0<sup>&#176;c</sup></h1>  
+ <p className=""><h5 className="mx-2 text-white lead" id="max"> max  </h5>  <h5 className="mx-1 lead text-white" id="min">min</h5> </p>
  
 </div>
-<h5 class="text-center text-white lead" id="city" >| city - name |</h5> 
- <p class="text-center my-3 d-flex justify-content-center align-items-center flex-wrap"> <span class="box p-2"> <i class="fa-solid fa-sun"></i> sun</span>  <span class="box p-2"> <i class="fa-solid fa-wind"></i>wind</span>  <span class="box p-2"><i class="fa-solid fa-cloud"></i> cloud</span>  <span class="box p-2"> <i class="fa-solid fa-cloud-rain"></i> rain</span>  </p> 
+<h5 className="text-center text-white lead" id="city" >| city - name |</h5> 
+ <p className="text-center my-3 d-flex justify-content-center align-items-center flex-wrap"> <span className="box p-2"> <i className="fa-solid fa-sun"></i> sun</span>  <span className="box p-2"> <i className="fa-solid fa-wind"></i>wind</span>  <span className="box p-2"><i className="fa-solid fa-cloud"></i> cloud</span>  <span className="box p-2"> <i className="fa-solid fa-cloud-rain"></i> rain</span>  </p> 
 
 
 
@@ -78,7 +78,7 @@ const Main = () => {
   {/* <h5 class="text-white lead">type the city name in the input ⬅️ and enter </h5> */}
   
   {/* <iframe class="px-2" style='height: 50vh; width: 100%;' frameborder="0" scrolling="no" src="https://maps.google.com/maps?q=${weatherdata.name}&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe> */}
-  <iframe width="460" height="473" src="https://maps.google.com/maps?q=sagar&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+  {/* <iframe width="460" height="473" src="https://maps.google.com/maps?q=sagar&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe> */}
    
   </div>
 </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css';
-import App from './weather/App';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-// import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+// import App from './weather/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import Main from './Main';
 
 ReactDOM.render(
